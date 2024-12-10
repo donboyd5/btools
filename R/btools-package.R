72b5e0e4434b58fea88d7a6448f46b827985eb6b
Don Boyd <donboyd5@gmail.com>
2024-12-10 06:44:50 -0500

    create btools-package.R

---
+++ R/btools-package.R
@@ -0,0 +1,8 @@
+#' btools: General text and data management tools
+#'
+#' The `btools` package includes utilities for working with strings, dates, and 
+#' numeric data, and utilities for dplyr.
+#'
+#' @docType package
+#' @name btools
+"_PACKAGE"
