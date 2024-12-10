318ee29c9ad3b12e3cc34ad3cc1ef156aaacc1ce
Don Boyd <donboyd5@gmail.com>
2024-12-10 06:30:21 -0500

    restyled file with styler::style_file()

--- R/btools_utilities.r
+++ R/btools_utilities.r
@@ -1,19 +1,13 @@
-# btools_utilities.r 
-# Don Boyd 1/22/2022
-
-# library(devtools)
-# library(btools)
+# btools_utilities.r
 
 # NOTE: put @export as first tag, NOT earlier.
-
-# usethis::use_pipe(export = TRUE)
 
 # numeric functions -------------------------------------------------------
 
 #' Convert NA to zero
 #'
 #' \code{naz} converts NA to zero
-#' 
+#'
 #' @export naz
 #' @usage naz(vec)
 #' @param vec The vector to convert.
@@ -23,31 +17,30 @@
 #' @examples
 #' naz(NA)
 naz <- function(vec) {
-    return(ifelse(is.na(vec), 0, vec))
+  return(ifelse(is.na(vec), 0, vec))
 }
 
 
 #' Convert Excel numeric date to date.
-#' 
+#'
 #' @export xldate
 #' @param xldate a numeric vector containing dates in Excel format.
 #' @return date
 #' @examples
 #' xldate(30000)
 xldate <- function(xldate) {
-    # convert Excel numeric date to date
-    date <- as.Date(as.numeric(xldate), origin = "1899-12-30")
-    return(date)
+  # convert Excel numeric date to date
+  date <- as.Date(as.numeric(xldate), origin = "1899-12-30")
+  return(date)
 }
-
 
 
 # Miscellaneous functions ----
 
 #' Factor to numeric
-#' 
+#'
 #' Returns a numeric vector, converted from factor.
-#' 
+#'
 #' @export fton
 #' @usage fton(fctr)
 #' @param fctr factor that we want to convert to numeric
@@ -57,155 +50,158 @@ xldate <- function(xldate) {
 #' @keywords fton
 #' @examples
 #' set.seed(1234)
-#' fctr <- factor(sample(1:4, 50, replace=TRUE), levels=1:4)
+#' fctr <- factor(sample(1:4, 50, replace = TRUE), levels = 1:4)
 #' fctr
 #' fton(fctr)
 fton <- function(fctr) {
-    as.numeric(levels(fctr)[fctr])
+  as.numeric(levels(fctr)[fctr])
 }
 
 
 #' Show head and tail of a vector, matrix, table, data frame or function
 #'
 #' \code{ht} head and tail of a vector, matrix, table, data frame or function
-#' 
+#'
 #' @export ht
 #' @usage ht(df, nrecs=6)
 #' @param df The object. No default.
 #' @param nrecs number of records, rows, whatever to show at head and at tail
-#' @details show head and tail of a vector, matrix, table, data frame or function
+#' @details show head and tail of a vector, matrix, table, data frame
+#'  or function
 #' @keywords ht
 #' @examples
 #' ht(mtcars, 4)
 ht <- function(df, nrecs = 6) {
-    print(utils::head(df, nrecs))
-    print(utils::tail(df, nrecs))
+  print(utils::head(df, nrecs))
+  print(utils::tail(df, nrecs))
 }
 
 
 #' which elements of a vector are even
-#' 
-#' @export is.even
-#' 
+#'
+#' @export is_even
+#'
 #' @param x vector.
 #' @return logical vector
 #' @examples
-#' is.even(1:10)
-is.even <- function(x) {(x %% 2) == 0}
+#' is_even(1:10)
+is_even <- function(x) {
+  (x %% 2) == 0
+}
 
 #' return even elements of a vector
-#' 
+#'
 #' @export even
-#' 
+#'
 #' @param x vector
 #' @return numeric vector
 #' @examples
 #' even(1:10)
-even <- function(x) {x[is.even(x)]}
+even <- function(x) {
+  x[is_even(x)]
+}
 
 
 #' which elements of a vector are odd
-#' 
-#' @export is.odd
-#' 
+#'
+#' @export is_odd
+#'
 #' @param x vector
 #' @return logical vector
 #' @examples
-#' is.odd(1:10)
-is.odd <- function(x) {!is.even(x)}
+#' is_odd(1:10)
+is_odd <- function(x) {
+  !is_even(x)
+}
 
 #' return odd elements of a vector
-#' 
+#'
 #' @export odd
-#' 
+#'
 #' @param x vector
 #' @return numeric vector
 #' @examples
 #' odd(1:10)
-odd <- function(x) {x[is.odd(x)]}
+odd <- function(x) {
+  x[is_odd(x)]
+}
 
 
 #' function to deal with NA logical values
-#' 
-#' @export is.true
+#'
+#' @export is_true
 #' @param x vector.
 #' @return logical vector
-is.true <- function(x) {
-    !is.na(x) & x
+is_true <- function(x) {
+  !is.na(x) & x
 }
 
 
 #' Describe memory usage and collect garbage
 #'
 #' \code{memory} describe memory usage and collect garbage
-#' 
+#'
 #' @export memory
 #' @usage memory(maxnobjs=5)
 #' @param maxnobjs The number of objects to display. Default is 5.
-#' @details Describes memory usage and collects garbage 
+#' @details Describes memory usage and collects garbage
 #' @keywords memory
 #' @examples
 #' memory(4)
 memory <- function(maxnobjs = 5) {
-    # function for getting the sizes of objects in memory
-    objs <- ls(envir = globalenv())
-    nobjs <- min(length(objs), maxnobjs)
-    
-    getobjs <- function() {
-        f <- function(x) utils::object.size(get(x))/1048600
-        sizeMB <- sapply(objs, f)
-        tmp <- data.frame(sizeMB)
-        tmp <- cbind(name = row.names(tmp), tmp) %>% dplyr::arrange(dplyr::desc(sizeMB))
-        # tmp <- tmp[order(-tmp$sizeMB), ]
-        row.names(tmp) <- NULL
-        tmp$sizeMB <- formatC(tmp$sizeMB, format = "f", digits = 2, big.mark = ",", preserve.width = "common")
-        return(tmp)
-    }
-    
-    print(paste0("Memory available: ", utils::memory.size(NA)))
-    print(paste0("Memory in use before: ", utils::memory.size()))
-    
-    if (nobjs > 0) {
-        print("Memory for selected objects: ")
-        print(utils::head(getobjs(), nobjs))
-    }
-    print(gc())
-    print(paste0("Memory in use after: ", utils::memory.size()))
+  # function for getting the sizes of objects in memory
+  objs <- ls(envir = globalenv())
+  nobjs <- min(length(objs), maxnobjs)
+
+  getobjs <- function() {
+    f <- function(x) utils::object.size(get(x)) / 1048600
+    size_mb <- sapply(objs, f)
+    tmp <- data.frame(size_mb)
+    tmp <- cbind(name = row.names(tmp), tmp) |>
+      dplyr::arrange(dplyr::desc(size_mb))
+    row.names(tmp) <- NULL
+    tmp$size_mb <- formatC(tmp$size_mb,
+      format = "f",
+      digits = 2,
+      big.mark = ",",
+      preserve.width = "common"
+    )
+    return(tmp)
+  }
+
+  print(paste0("Memory available: ", utils::memory.size(NA)))
+  print(paste0("Memory in use before: ", utils::memory.size()))
+
+  if (nobjs > 0) {
+    print("Memory for selected objects: ")
+    print(utils::head(getobjs(), nobjs))
+  }
+  print(gc())
+  print(paste0("Memory in use after: ", utils::memory.size()))
 }
 
 
-#' create vector of sorted names of a data frame
-#' 
+#' create vector of sorted names of an object
+#'
 #' @export ns
-#' @param df Data frame
+#' @param obj named object
 #' @return a vector of sorted names
 #' @examples
 #' names(iris) # unsorted
 #' ns(iris)
-ns <- function(df) {
-    names(df) %>% sort
-}
-
-# safe.ifelse may no longer be needed if using tidyr safely
-
-#' ifelse that can be used safely with dates
-#' 
-#' @export
-#' @param cond Logical expression.
-#' @param yes Resulting value if cond is TRUE.
-#' @param no Resulting value if cond is FALSE.
-#' @examples
-#' # snippet: mutate(date=safe.ifelse(freq=='A', as.Date(paste0(olddate, '-01-01')), date))
-safe.ifelse <- function(cond, yes, no) {
-    # so dates don't lose their class!
-    structure(ifelse(cond, yes, no), class = class(yes))
+ns <- function(obj) {
+  nms <- names(obj)
+  if (is.null(nms)) {
+    return(character(0))
+  }
+  sort(nms)
 }
 
 
 #' which elements are not in the intersection of two vectors?
-#' 
+#'
 #' @export
-#' 
+#'
 #' @param v1 vector
 #' @param v2 vector of the same type as v1
 #' @return a vector of items that are not in the intersection of two sets
@@ -217,6 +213,5 @@ safe.ifelse <- function(cond, yes, no) {
 #' setdiff(v1, v2)
 #' setdiff(v2, v1)
 setdiff_all <- function(v1, v2) {
-    setdiff(union(v1, v2), intersect(v1, v2))
+  setdiff(union(v1, v2), intersect(v1, v2))
 }
-
